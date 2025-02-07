b717490cc253db9f6ec5e6d9d06d4ca61aa8be1e
LAB303-24 <lab303-24@vdieng.local>
2025-02-07 16:52:53 +0700

    lab18_2 completed

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,14 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect* a, Rect* b) {
+    if (a->x + a->w <= b->x || b->x + b->w <= a->x)
+        return 0;
+    if (a->y - a->h >= b->y || b->y - b->h >= a->y)
+        return 0;
 
+    double width = (a->x + a->w < b->x + b->w ? a->x + a->w : b->x + b->w) - (a->x > b->x ? a->x : b->x);
+    double height = (a->y < b->y ? a->y : b->y) - (a->y - a->h > b->y - b->h ? a->y - a->h : b->y - b->h);
 
-
+    return width*height;
 }
